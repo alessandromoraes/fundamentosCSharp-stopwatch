e3801bdcc5db7b528996fa7b8454d1647953a656
Alessandro Moraes <alessandro.moraes07@gmail.com>
2021-07-03 18:04:02 -0300

    Commit Inicial

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Start(6);
+      }
+
+      static void Menu()
+      {
+         Console.Clear();
+         Console.WriteLine("");
+      }
+
+      static void Start(int time)
+      {
+         int currentTime = 0;
+
+         while (currentTime != time)
+         {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+         }
+
+         Console.Clear();
+         Console.WriteLine("Stopwatch finalizado");
+         Thread.Sleep(2500);
+      }
+   }
+}
